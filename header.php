94270f5fa464e1764c03c17e2c8872139600e5ec
Emmanuel Lechat <manu.lechat@free.fr>
2021-06-11 15:09:01 +0200

    wip

---
+++ header.php
@@ -0,0 +1,165 @@
+      <header class="main_header ">
+        <div class="main_header_left">
+          <div class="search_container">
+            <form class="search_form">
+              <input class="search_input txt_15" type="search" placeholder="Search..." aria-label="Search">
+              <button class="btn" type="submit">
+                <svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 589 520"
+                  style="enable-background:new 0 0 589 520;" xml:space="preserve">
+                  <g>
+                    <path d="M248.9,360.7c-76.4,0-138.5-62.1-138.5-138.5c0-76.4,62.1-138.5,138.5-138.5c76.4,0,138.5,62.1,138.5,138.5
+              C387.4,298.6,325.3,360.7,248.9,360.7z M248.9,133.7c-48.8,0-88.5,39.7-88.5,88.5c0,48.8,39.7,88.5,88.5,88.5s88.5-39.7,88.5-88.5
+              C337.4,173.4,297.7,133.7,248.9,133.7z" />
+                  </g>
+                  <g>
+                    <rect x="318" y="329" transform="matrix(0.7071 0.7071 -0.7071 0.7071 364.9628 -167.3577)"
+                      width="133" height="55.7" />
+                  </g>
+                </svg>
+              </button>
+            </form>
+          </div>
+          <div class="navbar_container">
+            <div class="navbar_line">
+
+              <span class="label ">View :</span>
+              <a class="view_item active" href="#">
+                <svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 520 520"
+                  style="enable-background:new 0 0 520 520;" xml:space="preserve">
+                  <g>
+                    <rect width="227.6" height="227.6" />
+                  </g>
+                  <g>
+                    <rect x="292.4" width="227.6" height="227.6" />
+                  </g>
+                  <g>
+                    <rect y="292.4" width="227.6" height="227.6" />
+                  </g>
+                  <g>
+                    <rect x="292.4" y="292.4" width="227.6" height="227.6" />
+                  </g>
+                </svg>
+              </a>
+              <a class="view_item" href="#">
+                <svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 589 520"
+                  style="enable-background:new 0 0 589 520;" xml:space="preserve">
+                  <g>
+                    <rect width="589" height="65.9" />
+                  </g>
+                  <g>
+                    <rect y="113.5" width="589" height="65.9" />
+                  </g>
+                  <g>
+                    <rect y="227" width="589" height="65.9" />
+                  </g>
+                  <g>
+                    <rect y="340.6" width="589" height="65.9" />
+                  </g>
+                  <g>
+                    <rect y="454.1" width="589" height="65.9" />
+                  </g>
+                </svg>
+              </a>
+              <a class="view_item" href="#">
+                <svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 520 520"
+                  style="enable-background:new 0 0 520 520;" xml:space="preserve">
+                  <circle cx="266.2" cy="254.4" r="116.3" />
+                  <g>
+                    <circle cx="442.5" cy="72.5" r="72.5" />
+                    <circle cx="90" cy="72.5" r="72.5" />
+                    <circle cx="442.5" cy="436.3" r="72.5" />
+                    <circle cx="90" cy="436.3" r="72.5" />
+                  </g>
+                  <rect x="45.8" y="239.5" transform="matrix(0.7071 0.7071 -0.7071 0.7071 257.8675 -113.7435)"
+                    width="440.9" height="29.7" />
+                  <rect x="45.8" y="239.5" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -101.911 262.7687)"
+                    width="440.9" height="29.7" />
+                </svg>
+              </a>
+              <div class="separator"></div>
+              <span class="label ">Sort by :</span>
+              <a class="sort_item active" href="#">
+                <span class="pin"></span>
+                Title
+              </a>
+              <a class="sort_item" href="#">
+                <span class="pin"></span>
+                Date
+              </a>
+              <div class="separator"></div>
+              <button type="button" class="btn btn-primary">+ new item</button>
+
+            </div>
+          </div>
+        </div>
+        <div class="main_header_right">
+
+          <a href="#" class="head_link">
+            <svg id="Calque_1" data-name="Calque 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34.04 37.66">
+              <g id="Groupe_284" data-name="Groupe 284">
+                <path id="Rectangle_174" data-name="Rectangle 174" class="cls-1"
+                  d="M3.49,32.86H30.58a2,2,0,0,1,2,2h0a2,2,0,0,1-2,2H3.49a2,2,0,0,1-2-2h0A2,2,0,0,1,3.49,32.86Z"
+                  transform="translate(-0.01 -0.01)" />
+                <path id="Rectangle_175" data-name="Rectangle 175" class="cls-1"
+                  d="M5.27,12.29h0A1.91,1.91,0,0,1,7.18,14.2V28.92a1.91,1.91,0,0,1-1.91,1.91h0a1.92,1.92,0,0,1-1.91-1.91V14.2A1.92,1.92,0,0,1,5.27,12.29Z"
+                  transform="translate(-0.01 -0.01)" />
+                <path id="Rectangle_176" data-name="Rectangle 176" class="cls-1"
+                  d="M13.11,12.29h0A1.91,1.91,0,0,1,15,14.2V28.92a1.91,1.91,0,0,1-1.91,1.91h0a1.92,1.92,0,0,1-1.91-1.91V14.2A1.92,1.92,0,0,1,13.11,12.29Z"
+                  transform="translate(-0.01 -0.01)" />
+                <path id="Rectangle_177" data-name="Rectangle 177" class="cls-1"
+                  d="M21,12.29h0a1.91,1.91,0,0,1,1.91,1.91V28.92A1.91,1.91,0,0,1,21,30.83h0A1.92,1.92,0,0,1,19,28.92V14.2A1.92,1.92,0,0,1,21,12.29Z"
+                  transform="translate(-0.01 -0.01)" />
+                <path id="Rectangle_178" data-name="Rectangle 178" class="cls-1"
+                  d="M28.79,12.29h0A1.91,1.91,0,0,1,30.7,14.2V28.92a1.91,1.91,0,0,1-1.91,1.91h0a1.92,1.92,0,0,1-1.91-1.91V14.2A1.92,1.92,0,0,1,28.79,12.29Z"
+                  transform="translate(-0.01 -0.01)" />
+                <path id="Tracé_95" data-name="Tracé 95" class="cls-1"
+                  d="M17,10.26H1.41a.64.64,0,0,1-.65-.64.66.66,0,0,1,.35-.57L7.92,5.47,16.73.83a.64.64,0,0,1,.6,0l8.81,4.64L33,9.05a.64.64,0,0,1-.3,1.21Z"
+                  transform="translate(-0.01 -0.01)" />
+              </g>
+            </svg>
+            Library
+          </a>
+
+
+
+          <div class="dropdown">
+            <div class="dropdown-toggle  head_link profil" type="button" id="dropdownMenuButton1"
+              data-bs-toggle="dropdown" aria-expanded="false">
+
+              <div class="img_container">
+                <img src="assets/images/fake.png" class="rounded-circle" alt="">
+              </div>
+              Manu Lechat
+
+
+            </div>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+
+              <form>
+                <div class="form_item">
+                  <label for="exampleInputEmail1" class="form-label txt_m">Email address</label>
+                  <input type="email" class="form-control form-control-sm" id="exampleInputEmail1"
+                    aria-describedby="emailHelp">
+                  <div id="emailHelp" class="form-text txt_s">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="form_item">
+                  <label for="exampleInputPassword1" class="form-label txt_m">Password</label>
+                  <input type="password" class="form-control form-control-sm" id="exampleInputPassword1">
+                </div>
+                <div class="form_item form-check">
+                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                  <label class="form-check-label txt_s" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+
+          </div>
+
+
+        </div>
+      </header>
---
+++ projet.php
@@ -0,0 +1,303 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/prospero.css">
+  <title>Prospero</title>
+</head>
+
+<body>
+
+  <main>
+    <section class="side_container">
+      <?php include('sidebar.php'); ?>
+    </section>
+    <section class="main_container">
+      <?php include('header.php'); ?>
+      <div class="main_content ">
+        <section class="row ">
+
+          <div class="col screenHeight">
+
+
+            <div class="card height_50">
+              <div class="card-header">
+                <div class="left">
+                  <h3 class="title txt_25">Corpora</h3>
+                </div>
+                <div class="right">
+                  <a href="#" class="icon_link edit"><i class="bi bi-pencil-fill"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16.992" height="16.973" viewBox="0 0 16.992 16.973">
+                      <g id="Groupe_122" data-name="Groupe 122" transform="translate(0 0)">
+                        <path id="Tracé_49" data-name="Tracé 49"
+                          d="M19.3,5.5h-.531L16.72,7.548l3.224,3.224,2.048-2.048V8.193ZM6.479,17.789,5,22.473l4.684-1.479L18.6,12.08,15.373,8.857Z"
+                          transform="translate(-5 -5.5)" fill="#707070" />
+                      </g>
+                    </svg>
+                  </a>
+                  <a href="#" class="icon_link plus">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="17.367" height="17.367" viewBox="0 0 17.367 17.367">
+                      <path id="Tracé_2" data-name="Tracé 2"
+                        d="M243.867,89.71h-6.71V83H233.21v6.71H226.5v3.947h6.71v6.71h3.947v-6.71h6.71Z"
+                        transform="translate(-226.5 -83)" fill="#707070" />
+                    </svg>
+                  </a>
+                  <a href="#" class="icon_link moins">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 4">
+                      <g id="Calque_2" data-name="Calque 2">
+                        <g id="Calque_1-2" data-name="Calque 1">
+                          <rect id="Rectangle_25" data-name="Rectangle 25" class="cls-1" width="17" height="4" />
+                        </g>
+                      </g>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+              <div class="card-body">
+                <?php include('table.php'); ?>
+              </div>
+            </div>
+
+            <div class="card height_25">
+              <div class="card-header">
+                <div class="left">
+                  <h3 class="title txt_25">titre</h3>
+                </div>
+                <div class="right">
+                  <a href="#" class="icon_link edit"><i class="bi bi-pencil-fill"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16.992" height="16.973" viewBox="0 0 16.992 16.973">
+                      <g id="Groupe_122" data-name="Groupe 122" transform="translate(0 0)">
+                        <path id="Tracé_49" data-name="Tracé 49"
+                          d="M19.3,5.5h-.531L16.72,7.548l3.224,3.224,2.048-2.048V8.193ZM6.479,17.789,5,22.473l4.684-1.479L18.6,12.08,15.373,8.857Z"
+                          transform="translate(-5 -5.5)" fill="#707070" />
+                      </g>
+                    </svg>
+                  </a>
+                  <a href="#" class="icon_link plus">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="17.367" height="17.367" viewBox="0 0 17.367 17.367">
+                      <path id="Tracé_2" data-name="Tracé 2"
+                        d="M243.867,89.71h-6.71V83H233.21v6.71H226.5v3.947h6.71v6.71h3.947v-6.71h6.71Z"
+                        transform="translate(-226.5 -83)" fill="#707070" />
+                    </svg>
+                  </a>
+                  <a href="#" class="icon_link moins">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 4">
+                      <g id="Calque_2" data-name="Calque 2">
+                        <g id="Calque_1-2" data-name="Calque 1">
+                          <rect id="Rectangle_25" data-name="Rectangle 25" class="cls-1" width="17" height="4" />
+                        </g>
+                      </g>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+              <div class="card-body">
+                <?php include('table.php'); ?>
+              </div>
+            </div>
+
+            <div class="card height_25">
+              <div class="card-header">
+                <div class="left">
+                  <h3 class="title txt_25">titre</h3>
+                </div>
+                <div class="right">
+                  <a href="#" class="icon_link edit"><i class="bi bi-pencil-fill"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16.992" height="16.973" viewBox="0 0 16.992 16.973">
+                      <g id="Groupe_122" data-name="Groupe 122" transform="translate(0 0)">
+                        <path id="Tracé_49" data-name="Tracé 49"
+                          d="M19.3,5.5h-.531L16.72,7.548l3.224,3.224,2.048-2.048V8.193ZM6.479,17.789,5,22.473l4.684-1.479L18.6,12.08,15.373,8.857Z"
+                          transform="translate(-5 -5.5)" fill="#707070" />
+                      </g>
+                    </svg>
+                  </a>
+                  <a href="#" class="icon_link plus">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="17.367" height="17.367" viewBox="0 0 17.367 17.367">
+                      <path id="Tracé_2" data-name="Tracé 2"
+                        d="M243.867,89.71h-6.71V83H233.21v6.71H226.5v3.947h6.71v6.71h3.947v-6.71h6.71Z"
+                        transform="translate(-226.5 -83)" fill="#707070" />
+                    </svg>
+                  </a>
+                  <a href="#" class="icon_link moins">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 4">
+                      <g id="Calque_2" data-name="Calque 2">
+                        <g id="Calque_1-2" data-name="Calque 1">
+                          <rect id="Rectangle_25" data-name="Rectangle 25" class="cls-1" width="17" height="4" />
+                        </g>
+                      </g>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+              <div class="card-body">
+
+                <div class="cartouche txt_15">
+                  <div class="cartouche_item"><label>Owner: </label>Le vaccin contre le Covid-19 sera-t-il obligatoire ?</div>
+                  <div class="cartouche_item"><label>Permissions: </label>Read & Write</div>
+                  <div class="cartouche_item"><label>Tags: </label>Covid-19, vaccin, gouvernement, pandémie, …</div>
+                  <div class="cartouche_item"><label>Number of text: </label>May 25, 2020</div>
+                  <div class="cartouche_item"><label>Number of media text: </label>Internet</div>
+                  <div class="cartouche_item"><label>Commentaries: </label>Researches about something</div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col screenHeight">
+            <div class="card height_100">
+              <div class="card-header">
+                <div class="left">
+                  <h3 class="title txt_25">titre</h3>
+                </div>
+                <div class="right">
+                  <a href="#" class="icon_link edit"><i class="bi bi-pencil-fill"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16.992" height="16.973" viewBox="0 0 16.992 16.973">
+                      <g id="Groupe_122" data-name="Groupe 122" transform="translate(0 0)">
+                        <path id="Tracé_49" data-name="Tracé 49"
+                          d="M19.3,5.5h-.531L16.72,7.548l3.224,3.224,2.048-2.048V8.193ZM6.479,17.789,5,22.473l4.684-1.479L18.6,12.08,15.373,8.857Z"
+                          transform="translate(-5 -5.5)" fill="#707070" />
+                      </g>
+                    </svg>
+                  </a>
+                  <a href="#" class="icon_link plus">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="17.367" height="17.367" viewBox="0 0 17.367 17.367">
+                      <path id="Tracé_2" data-name="Tracé 2"
+                        d="M243.867,89.71h-6.71V83H233.21v6.71H226.5v3.947h6.71v6.71h3.947v-6.71h6.71Z"
+                        transform="translate(-226.5 -83)" fill="#707070" />
+                    </svg>
+                  </a>
+                  <a href="#" class="icon_link moins">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 4">
+                      <g id="Calque_2" data-name="Calque 2">
+                        <g id="Calque_1-2" data-name="Calque 1">
+                          <rect id="Rectangle_25" data-name="Rectangle 25" class="cls-1" width="17" height="4" />
+                        </g>
+                      </g>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+              <div class="card-body">
+
+
+                <?php include('table.php'); ?>
+
+              </div>
+            </div>
+          </div>
+
+
+
+
+          <div class="col screenHeight">
+            <div class="card height_100">
+              <div class="card-header">
+                <div class="left">
+                  <h3 class="title txt_25">titre</h3>
+                </div>
+                <div class="right">
+                  <a href="#" class="icon_link edit"><i class="bi bi-pencil-fill"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16.992" height="16.973" viewBox="0 0 16.992 16.973">
+                      <g id="Groupe_122" data-name="Groupe 122" transform="translate(0 0)">
+                        <path id="Tracé_49" data-name="Tracé 49"
+                          d="M19.3,5.5h-.531L16.72,7.548l3.224,3.224,2.048-2.048V8.193ZM6.479,17.789,5,22.473l4.684-1.479L18.6,12.08,15.373,8.857Z"
+                          transform="translate(-5 -5.5)" fill="#707070" />
+                      </g>
+                    </svg>
+                  </a>
+                  <a href="#" class="icon_link plus">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="17.367" height="17.367" viewBox="0 0 17.367 17.367">
+                      <path id="Tracé_2" data-name="Tracé 2"
+                        d="M243.867,89.71h-6.71V83H233.21v6.71H226.5v3.947h6.71v6.71h3.947v-6.71h6.71Z"
+                        transform="translate(-226.5 -83)" fill="#707070" />
+                    </svg>
+                  </a>
+                  <a href="#" class="icon_link moins">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 4">
+                      <g id="Calque_2" data-name="Calque 2">
+                        <g id="Calque_1-2" data-name="Calque 1">
+                          <rect id="Rectangle_25" data-name="Rectangle 25" class="cls-1" width="17" height="4" />
+                        </g>
+                      </g>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+              <div class="card-body">
+
+                <div class="cartouche txt_15">
+                  <div class="cartouche_item"><label>Title: </label>Le vaccin contre le Covid-19 sera-t-il obligatoire
+                    ?</div>
+                  <div class="cartouche_item"><label>Author: </label>Jean-Yves Nau</div>
+                  <div class="cartouche_item"><label>Source: </label>Slate</div>
+                  <div class="cartouche_item"><label>Date: </label>May 25, 2020</div>
+                  <div class="cartouche_item"><label>Support: </label>Internet</div>
+                  <div class="cartouche_item"><label>Number of characters: </label>200 000</div>
+                </div>
+                <article class="main_article">
+                  <p>
+                    Formidable paradoxe: alors qu’on ne cesse d’évoquer les bénéfices d’un vaccin protecteur contre le
+                    Covid-19, les ventes de vaccins ne cessent, en France de diminuer; atteignant, durant le
+                    confinement, moins 50% pour l’obligatoire rougeole-oreillons-rubéole. Conséquence: un risque
+                    croissant de résurgence épidémique de maladies infectieuses et contagieuses. Un risque d’autant
+                    plus grand qu’il s’inscrit dans le cadre plus général d’une opposition, à la fois latente et
+                    structurée, au principe même de la vaccination.
+                  </p>
+                  <p>
+                    Dans ce contexte, on peut prévoir l’émergence sous une nouvelle forme, de la confrontation entre
+                    les libertés individuelles et un pouvoir exécutif imposant une contrainte corporelle au nom de la
+                    santé publique. Imposer, de gré ou de force, le futur vaccin contre le Covid-19?
+                  </p>
+                  <p>
+                    Cette confrontation bien connue, qui nourrit le combat des anti-vaccins, prendra alors une
+                    nouvelle dimension. À la fois du fait de la dimension pandémique de la maladie, mais aussi et
+                    surtout de ses innombrables conséquences médicales et économiques, politiques et diplomatiques.
+                  </p>
+                  <p>
+                    Il n’est pas trop tôt pour s’intéresser à ce sujet. Si certaines interrogations théoriques
+                    demeurent, tout laisse penser, au vu des efforts considérables déployés à l’échelle international,
+                    que l’on parviendra à mettre au point plusieurs vaccins protecteurs contre le Covid-19; les
+                    premiers pouvant être mis sur le marché durant l’année 2021.
+                  </p>
+                  <p>
+                    Or un sondage Ifop pour le consortium Coconel (Coronavirus et confinement: enquête longitudinale)
+                    réalisé fin mars 2020 établit que plus d’un quart des Français·es (26%) ne voudraient pas se faire
+                    vacciner contre le SARS-CoV-2. «Ce refus est plus important chez les femmes, notamment les jeunes,
+                    alors que ce sont elles, souvent, qui prennent les décisions vaccinales pour les enfants», pointe
+                    dans Le Monde Patrick Peretti-Watel, sociologue, directeur de recherche à l’Inserm et coordinateur
+                    scientifique du projet Coconel.
+                  </p>
+                  <p>
+                    «Le pourcentage de refus est plus élevé (39%) chez les 26-35 ans. Un tiers environ des employés et
+                    des ouvriers le refuseraient aussi, alors que, chez les cadres et professions intellectuelles
+                    supérieures, le taux de refus n’est que de 16%.»
+                </article>
+
+
+              </div>
+            </div>
+          </div>
+
+        </section>
+
+
+      </div>
+
+    </section>
+  </main>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
