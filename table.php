46961a5480b8b6459aa4cb004fc9af705bea73a8
Emmanuel Lechat <manu.lechat@free.fr>
2021-09-09 17:20:35 +0200

    up

--- table.php
+++ table.php
@@ -1,11 +1,39 @@
           <table class="table table-hover">
-            <thead>
-              <tr>
-                <th>Firstname</th>
-                <th>Lastname</th>
-                <th>Email</th>
-              </tr>
-            </thead>
+           <thead>
+        <tr>
+            <th property-name="name">
+                <div class="table-col-title">
+                    Name
+                    <span class="sort-filters">
+    <span class="sort-filter sort-filter-asc active"></span>
+    <!--<span class="sort-filter sort-filter-desc">-</span>-->
+</span>
+
+                </div>
+            </th>
+            <th property-name="author">
+                <div class="table-col-title">
+                    Author
+                    <span class="sort-filters">
+    <span class="sort-filter sort-filter-asc"></span>
+    <!--<span class="sort-filter sort-filter-desc">-</span>-->
+</span>
+
+                </div>
+
+            </th>
+            <th property-name="type">
+                <div class="table-col-title">
+                    Type
+                    <span class="sort-filters">
+    <span class="sort-filter sort-filter-asc"></span>
+    <!--<span class="sort-filter sort-filter-desc">-</span>-->
+</span>
+
+                </div>
+            </th>
+        </tr>
+    </thead>
             <tbody>
               <tr>
                 <td>John</td>
